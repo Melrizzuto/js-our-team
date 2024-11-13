a60fac5916856f043e0c45dc0486e34e8f7ddcbe
Melrizzuto <melrizzuto4@gmail.com>
2024-11-13 15:17:41 +0100

    modified mq js

--- script/script.js
+++ script/script.js
@@ -40,24 +40,28 @@ const teamMembers = [
 const teamContainer = document.getElementById("team-cards");
 
 // Creo le cards
-teamMembers.forEach(member => {
-  const memberCard = document.createElement("div");
-  memberCard.className = "col-12 col-sm-6 col-md-4 col-lg-4 mb-4"; // Responsività
-  memberCard.innerHTML = `
-    <div class="card text-bg-dark mb-3">
-      <div class="row g-0">
-        <div class="col-4">
-          <img src="${member.img}" class="img-fluid rounded-start" alt="${member.name}">
-        </div>
-        <div class="col-8">
-          <div class="card-body">
-            <h5 class="card-title">${member.name}</h5>
-            <p class="card-text">${member.role}</p>
-            <a href="mailto:${member.email}" class="card-text">${member.email}</a>
+let teamHTML = ''; // Variabile per accumulare il contenuto HTML
+
+for (const member of teamMembers) {
+  teamHTML += `
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4 mb-4"> <!-- Responsività -->
+      <div class="card text-bg-dark mb-3">
+        <div class="row g-0">
+          <div class="col-4">
+            <img src="${member.img}" class="img-fluid rounded-start" alt="${member.name}">
+          </div>
+          <div class="col-8">
+            <div class="card-body">
+              <h5 class="card-title text-uppercase">${member.name}</h5>
+              <p class="card-text">${member.role}</p>
+              <a href="mailto:${member.email}" class="card-text">${member.email}</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
   `;
-  teamContainer.appendChild(memberCard);
-});
+}
+
+// Impostiamo tutto il contenuto HTML accumulato nel container
+teamContainer.innerHTML = teamHTML;
